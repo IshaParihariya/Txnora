935d5e01e4fdc62e391cb5d317bc96440b698ae2
isha parihariya <ishh1036@gmail.com>
2026-09-01 16:09:16 +0530

    first commit

---
+++ src/main/java/com/example/txnora/TxnoraApplication.java
@@ -0,0 +1,13 @@
+package com.example.txnora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxnoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TxnoraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/txnora/TxnoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.txnora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxnoraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
