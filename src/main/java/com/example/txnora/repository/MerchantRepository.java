f0ec16e4e0aba08009ff3a6ee60921fd4a373ec1
isha parihariya <ishh1036@gmail.com>
2026-09-03 23:07:58 +0530

    changes made

--- src/main/java/com/example/txnora/repository/MerchantRepository.java
+++ src/main/java/com/example/txnora/repository/MerchantRepository.java
@@ -2,8 +2,6 @@ package com.example.txnora.repository;
 
 import com.example.txnora.model.Merchant;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public class MerchantRepository extends MongoRepository<Merchant, String> {
+public interface MerchantRepository extends MongoRepository<Merchant, String> {
 }
